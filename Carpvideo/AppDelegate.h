65d0602b6ff9384caf066da8511eae2744e7f52c
huzhixin <593877243@qq.com>
2021-06-24 18:02:08 +0800

    合并代码

--- Carpvideo/AppDelegate.h
+++ Carpvideo/AppDelegate.h
@@ -7,9 +7,15 @@
 
 #import <UIKit/UIKit.h>
 #import "CarpVideoBaseTabbarViewController.h"
+#import "ORTabBarController.h"
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 @property(nonatomic,strong) UIWindow * window;
 @property(nonatomic,strong) CarpVideoBaseTabbarViewController * CarpVideoTabbar;
+@property (nonatomic, assign) BOOL isShouldOrientationMaskAll;
+@property (nonatomic, weak, readonly) ORTabBarController *tabBarViewController;
+/**< RootViewController */
+
+
 + (AppDelegate *)shareDelegate;
 
 @end
